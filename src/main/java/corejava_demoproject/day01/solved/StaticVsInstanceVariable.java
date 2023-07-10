463d63bcbb65a2647a177f4afd78bec6309c9594
uthra121 <116268900+uthra121@users.noreply.github.com>
2023-07-10 16:57:11 +0530

    finish day01 task

---
+++ src/main/java/corejava_demoproject/day01/solved/StaticVsInstanceVariable.java
@@ -0,0 +1,29 @@
+package corejava_demoproject.day01.solved;
+
+public class StaticVsInstanceVariable {
+   
+	// Declare a static variable
+    public static int staticVariable;
+    
+    // Declare an instance variable
+    public int instanceVariable;
+
+    public static void main(String[] args) {
+       
+    	/////For Static Variable
+    	
+    	// Access the static variable using the class name
+    	StaticVsInstanceVariable.staticVariable = 10;
+        System.out.println("Static Variable: " + StaticVsInstanceVariable.staticVariable);
+        
+        /////  For Instance variable
+        
+     // Create an object of MyClass
+        StaticVsInstanceVariable myObject = new StaticVsInstanceVariable();
+
+        // Access the instance variable using the object reference
+        myObject.instanceVariable = 5;
+        System.out.println("Instance Variable: "+ myObject.instanceVariable);
+        
+    }
+}
